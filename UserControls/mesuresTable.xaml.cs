0782bd9296a01973fe441e79f2ec00e82f4d7fff
yazidSRD <sardi.yazid@gmail.com>
2023-04-12 09:10:02 +0200

    v42
    Nouveau système de téléchargement en fichier XML plus performant

--- UserControls/mesuresTable.xaml.cs
+++ UserControls/mesuresTable.xaml.cs
@@ -74,37 +74,48 @@ namespace projet23_Station_météo_WPF.UserControls
         }
         void Button_Click_Download(object sender, RoutedEventArgs e)
         {
-            string contenu = "<?xml version='1.0' encoding='UTF-8'?>\n";
-            
-            contenu += "<mesures>\n";
-
-            foreach (Dictionary<string, string> item in dataGrid.Items)
-            {
-                contenu += "\t<mesure>\n";
-                contenu += "\t\t<DateHeureReleve>"+ item["DateHeureReleve"] + "</DateHeureReleve>\n";
-                contenu += "\t\t<Temperature>" + item["Temperature"] + "</Temperature>\n";
-                contenu += "\t\t<Hygrometrie>" + item["Hygrometrie"] + "</Hygrometrie>\n";
-                contenu += "\t\t<VitesseVent>" + item["VitesseVent"] + "</VitesseVent>\n";
-                contenu += "\t\t<DirectionVent>" + item["DirectionVent"] + "</DirectionVent>\n";
-                contenu += "\t\t<PressionAtmospherique>" + item["PressionAtmospherique"] + "</PressionAtmospherique>\n";
-                contenu += "\t\t<Pluviometre>" + item["Pluviometre"] + "</Pluviometre>\n";
-                contenu += "\t\t<RayonnementSolaire>" + item["RayonnementSolaire"] + "</RayonnementSolaire>\n";
-                contenu += "\t</mesure>\n";
-            }
-
-            contenu += "</mesures>";
-
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.Filter = "langage de balisage extensible (*.xml)|*.xml";
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
                 using (StreamWriter sw = File.CreateText(saveFileDialog.FileName))
                 {
+                    string contenu = "<?xml version='1.0' encoding='UTF-8'?>\n";
+
+                    contenu += "<mesures>\n";
+                    sw.Write(contenu);
+                    
+                    int total = 0;
+                    int x = 0;
+                    contenu = "";
+                    foreach (Dictionary<string, string> item in dataGrid.Items)
+                    {
+                        if (x++>200)
+                        {
+                            sw.Write(contenu);
+                            contenu = "";
+                        }
+                        Console.WriteLine(total++ + "/" + dataGrid.Items.Count);
+                        contenu += "\t<mesure>\n";
+                        contenu += "\t\t<DateHeureReleve>" + item["DateHeureReleve"] + "</DateHeureReleve>\n";
+                        contenu += "\t\t<Temperature>" + item["Temperature"] + "</Temperature>\n";
+                        contenu += "\t\t<Hygrometrie>" + item["Hygrometrie"] + "</Hygrometrie>\n";
+                        contenu += "\t\t<VitesseVent>" + item["VitesseVent"] + "</VitesseVent>\n";
+                        contenu += "\t\t<DirectionVent>" + item["DirectionVent"] + "</DirectionVent>\n";
+                        contenu += "\t\t<PressionAtmospherique>" + item["PressionAtmospherique"] + "</PressionAtmospherique>\n";
+                        contenu += "\t\t<Pluviometre>" + item["Pluviometre"] + "</Pluviometre>\n";
+                        contenu += "\t\t<RayonnementSolaire>" + item["RayonnementSolaire"] + "</RayonnementSolaire>\n";
+                        contenu += "\t</mesure>\n";
+                    }
+                    if (x!=0) sw.Write(contenu);
+
+                    contenu = "</mesures>";
                     sw.Write(contenu);
                 }
 
                 Console.WriteLine("Le fichier texte a été créé avec succès et enregistré à l'emplacement suivant : " + saveFileDialog.FileName);
             }
+            else return;
         }
     }
 }
